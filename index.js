ef72eb43172a1dc4c34bf229e21e1517b2f21c63
hashamuddin1 <hashamlaptop@gmail.com>
2024-03-02 14:43:49 +0500

    teach about env and nodemon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+require("dotenv").config();
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
